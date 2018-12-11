5e7238f916331e42d6fa3794717922cffe0af138
Michael Vaganov <mvaganov@hotmail.com>
2018-12-10 17:03:46 -0800

    OMU.Util
    made a Util static class, which has clearer use-cases for OMU script serialization/deserialization

---
+++ galactus/Assets/Nonstandard Assets/OMU/Util.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OMU {
+	public static class Util {
+		public static object FromScript(string omScript) {
+			return FromScript(omScript, "unnamed script");
+		}
+		public static object FromScript(string omScript, string sourceName) {
+			FileParseResults results = null;
+			return Parser.Parse(Parser.ParseType.JSON, sourceName, omScript, ref results);
+		}
+
+		public static string ToScript(object obj) {
+			return Serializer.Stringify(obj, "\t", false, false, null);
+		}
+
+		public static string ToScriptTiny(object obj) {
+			return Serializer.StringifyTiny(obj);
+		}
+
+		public static TYPE FromScriptOverwrite<TYPE>(string omScript, ref TYPE objectToOverwrite) where TYPE : class {
+			return FromScriptOverwrite(omScript, ref objectToOverwrite, null, "unnamed script");
+		}
+		public static TYPE FromScriptOverwrite<TYPE>(string omScript, ref TYPE objectToOverwrite, FileParseResults results, string sourceName) where TYPE : class {
+			FileParseResults resultsEvenIfUserDidntAskForThem = (results != null) ? results : new FileParseResults(sourceName, omScript);
+			do {
+				// parse the JSON tree
+				object dom = Parser.Parse(Parser.ParseType.JSON, sourceName, omScript, ref resultsEvenIfUserDidntAskForThem);
+				if(dom == null) break; // if parsing failed, we're done.
+				System.Type t = typeof(TYPE);
+				if(dom.GetType() == t) { objectToOverwrite = dom as TYPE; break; }// if parsing created the type being searched for, we're done
+				if(objectToOverwrite == null) {
+					objectToOverwrite = Data.CreateNew(t) as TYPE; // otherwise, construct and populate data as requested
+				}
+				object output = objectToOverwrite;
+				string errorText = Data.SetObjectFromOm(ref output, dom as object, Data.JSONFieldSearchBehavior.startswith, null);
+				if(errorText != null) {
+					resultsEvenIfUserDidntAskForThem.ERROR(errorText, Coord.INVALID);
+				}
+			} while(false);
+			if(resultsEvenIfUserDidntAskForThem.Count > 0 && results == null) {
+				Debug.LogError(resultsEvenIfUserDidntAskForThem);
+			}
+			return objectToOverwrite;
+		}
+	}
+}
--- galactus/Assets/TESTING/OM_test.cs
+++ galactus/Assets/TESTING/OM_test.cs
@@ -4,18 +4,33 @@ using UnityEngine;
 
 public class OM_test : MonoBehaviour {
 
-    [TextArea(3, 5)]
-    public string input = "{\n"+
-        "a:5,"+
-        "text:\'Hello World!\'\n"+
-        "}";
+	[TextArea(3, 5)]
+	public string input = "{\n"+
+		"a:5, \n"+
+		"text:\'Hello World!\'\n"+
+		"}";
+
+	public class TestClass {
+		public int a;
+		public string text;
+	}
 
 	// Use this for initialization
 	void Start () {
-        OMU.Value ob = OMU.Value.FromScript(input);
-        Debug.Log(OMU.Serializer.Stringify(ob.GetRawObject()));
+		object ob = OMU.Util.FromScript(input);
+		Debug.Log(OMU.Util.ToScript(ob));
+
+		object tc = new TestClass();
+		OMU.Util.FromScriptOverwrite(input, ref tc);
+		string scripted = OMU.Util.ToScriptTiny(tc);
+		Debug.Log(scripted);
+
+		ob = OMU.Util.FromScript(scripted);
+		tc = ob as TestClass;
+		Debug.Log(ob.GetType());
+		Debug.Log(OMU.Util.ToScript(tc));
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
